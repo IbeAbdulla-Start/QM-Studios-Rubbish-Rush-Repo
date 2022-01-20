40bd2b18a8c2c063cd0e4f06f9c0398aaf9830ba
Abdalla Mohamed <abdulla.mohamed.university@gmail.com>
2022-01-20 18:06:11 -0500

    Previous work

---
+++ OTTER-master/projects/Rubbish Rush Engine/src/Gameplay/Components/JumpBehaviour.cpp
@@ -0,0 +1,57 @@
+#include "Gameplay/Components/JumpBehaviour.h"
+#include "Gameplay/Components/ComponentManager.h"
+#include "Utils/GlmBulletConversions.h"
+#include "Gameplay/Components/IComponent.h"
+#include <GLFW/glfw3.h>
+#include "Gameplay/Physics/RigidBody.h"
+#include "Gameplay/GameObject.h"
+#include "Gameplay/Scene.h"
+#include "Utils/ImGuiHelper.h"
+
+
+void JumpBehaviour::Awake()
+{
+	_body = GetComponent<Gameplay::Physics::RigidBody>();
+	if (_body == nullptr) {
+		IsEnabled = false;
+	}
+}
+
+void JumpBehaviour::RenderImGui() {
+	LABEL_LEFT(ImGui::DragFloat, "Impulse", &_impulse, 1.0f);
+}
+
+nlohmann::json JumpBehaviour::ToJson() const {
+	return {
+		{ "impulse", _impulse }
+	};
+}
+
+JumpBehaviour::JumpBehaviour() :
+	IComponent(),
+	_impulse(5.0f)
+{ }
+
+JumpBehaviour::~JumpBehaviour() = default;
+
+JumpBehaviour::Sptr JumpBehaviour::FromJson(const nlohmann::json& blob) {
+	JumpBehaviour::Sptr result = std::make_shared<JumpBehaviour>();
+	result->_impulse = blob["impulse"];
+	return result;
+}
+
+void JumpBehaviour::Update(float deltaTime) {
+	bool pressed = glfwGetKey(GetGameObject()->GetScene()->Window, GLFW_KEY_SPACE);
+	timer += deltaTime;
+	if (pressed) {
+		if (_isPressed == false && timer >= cooldownTime) {
+			_body->ApplyImpulse(glm::vec3(0.0f, 0.0f, _impulse));
+			timer = 0.0f;
+		}
+		_isPressed = pressed;
+	} 
+	else{
+		_isPressed = false;
+	}
+}
+
